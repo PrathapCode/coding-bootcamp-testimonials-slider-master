b52ae9de95c7d27c5d7b1f21e96d1a2fa4789e66
Prathap <101249786+PrathapCode@users.noreply.github.com>
2022-07-21 16:25:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+// document.querySelector(".left-arrow").addEventListener("click" , function() {
+//     document.querySelector(".tanya").style.visibility = "visible";
+//     document.querySelector(".john").style.visibility = "hidden";
+// })
+
+// document.querySelector(".right-arrow").addEventListener("click" ,function() {
+//     document.querySelector(".tanya").style.visibility = "hidden";
+//     document.querySelector(".john").style.visibility = "visible";
+// })
